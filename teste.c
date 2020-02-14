bae68cdaae1560dde7bf5757daecf7b47b0f2880
tereski30 <herbert_tereskii@hotmail.com>
2020-02-14 10:05:42 -0300

    Impar ou par

--- teste.c
+++ teste.c
@@ -3,6 +3,17 @@
 
 int main(){
 
+	int num;
+
+	printf("Digite um numero:\n");
+	scanf("%i", &num);
+
+	if(num % 2 == 0){
+		printf("O numero %i eh par.", num);
+	}
+	else{
+		printf("O numero %i eh impar.", num);
+	}
 
     return 0;
 }
